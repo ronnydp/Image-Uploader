22cd08ce7ac3f82c73d6fc41f5aaa188866e3f7b
ronnydp <ronnydp742@gmail.com>
2024-11-26 21:50:18 -0500

    update 1.5

--- src/public/js/dropDrag.js
+++ src/public/js/dropDrag.js
@@ -1,14 +1,18 @@
-const input = document.getElementById('input-file');
-const dropArea = document.getElementById('dropArea');
+const inputFile = document.getElementById('input-file');
+const dragArea = document.getElementById('drop-area');
 
-input.addEventListener("change", uploadImage) ;
+inputFile.addEventListener("change", uploadImage);
 
 function uploadImage(){
-    let imgLink = URL.createObjectURL(input.files[0]);
-    dropArea.style.backgroundImage = `url(${imgLink})`;
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.addEventListener
 }
 
-// Agregar evento de clic al dropArea
-dropArea.addEventListener("click", function() {
-    input.click(); // Simula un clic en el input de archivo
+dragArea.addEventListener("dragover", function(e){
+    e.preventDefault();
 });
+
+dragArea.addEventListener("drop", function(e){
+    e.preventDefault();
+    
+})
